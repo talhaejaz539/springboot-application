eeb6349cfc286def4d3939b78360eaf7ca5333c6
talhaejaz539 <talhaejazup@gmail.com>
2024-04-01 21:13:52 +0500

    Receipt Module Completed

--- src/main/java/com/zbhg/mcp/firstProject/models/SaleLineItemRepository.java
+++ src/main/java/com/zbhg/mcp/firstProject/models/SaleLineItemRepository.java
@@ -3,6 +3,9 @@ package com.zbhg.mcp.firstProject.models;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface SaleLineItemRepository extends JpaRepository<SaleLineItemEntity, Integer> {
 
+    List<SaleLineItemEntity> findAllByOrderId(Integer integer);
 }
